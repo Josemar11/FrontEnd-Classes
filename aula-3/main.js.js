8a2c2e284452e907822a028192340c8157de929c
Josemar <93160986+Josemar11@users.noreply.github.com>
2022-06-01 20:16:44 -0300

    First commit

---
+++ aula-3/main.js.js
@@ -0,0 +1,6 @@
+var jogosDigitaisSection = document.querySelector()
+console.log(document.querySelector('section'))
+console.log(document.querySelector('#firstPost'))
+console.log(document.querySelectorAll('.post'))
+
+console.log(document.querySelector('#integrantesGrupo'))
