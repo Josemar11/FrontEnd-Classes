3ed123658318e89907e99ce52c51622819cdd9ae
Josemar <93160986+Josemar11@users.noreply.github.com>
2022-06-24 11:33:23 -0300

    Split

---
+++ Manipulacao de Dados - Split/main.js
@@ -0,0 +1,10 @@
+// Separa uma string por seus caracteres ou várias strings em valores de array.
+
+var mensagem = "Olá mundo, sou um texto!";
+var palavras = mesagem.split(" ");
+// palavras = ["Olá", "Mundo", "sou", "um", "texto!"];
+
+var palavra = "Olá";
+var letras = palavra.split("");
+// letras = ["O", "l", "á"]
+
